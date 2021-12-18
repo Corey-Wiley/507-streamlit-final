aa6d921805981fdfd337f6fa415c154e6595507d
Corey Wiley <corey.wiley@stonybrook.edu>
2021-12-17 23:45:09 -0500

    update

--- 507streamlitFinal.py
+++ 507streamlitFinal.py
@@ -14,5 +14,27 @@ from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import time
 
-st.title('Uber pickups in NYC')
+@st.cache
+def load_hospitals():
+    df_hospital = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/hospital_info.csv')
+    return df_hospital
 
+@st.cache
+def load_inatpatient():
+    df_inpatient = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/inpatient_2015.csv')
+    return df_inpatient
+
+@st.cache
+def load_outpatient():
+    df_outpatient = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/outpatient_2015.csv')
+    return df_outpatient
+
+# FAKE LOADER BAR TO STIMULATE LOADING    
+# my_bar = st.progress(0)
+# for percent_complete in range(100):
+#     time.sleep(0.1)
+#     my_bar.progress(percent_complete + 1)
+
+df_hospital = load_hospitals()
+df_inpatient = load_inatpatient()
+df_outpatient = load_outpatient()
