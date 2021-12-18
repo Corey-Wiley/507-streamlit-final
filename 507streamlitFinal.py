086d669ca3a27d412035440ac55071d536cb9a3d
Corey Wiley <corey.wiley@stonybrook.edu>
2021-12-17 23:21:36 -0500

    streamlit final

--- 507streamlitFinal.py
+++ 507streamlitFinal.py
@@ -6,3 +6,13 @@ Created on Fri Dec 17 17:34:56 2021
 @author: coreywiley
 """
 
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import time
+
+st.title('Uber pickups in NYC')
+
